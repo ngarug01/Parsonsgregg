93c67fdc91d506fb81e01208d9f93a1511c8dbba
Jason.Whitworth <jason.whitworth@ten10.com>
2018-12-03 15:47:50 +0000

    Added acceptance test for 'Hello World!'

--- acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/HelloWorld.java
+++ acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/HelloWorld.java
@@ -1,4 +1,35 @@
 package com.ten10.training.javaparsons.acceptancetests;
 
-public class HelloWorld {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+
+class HelloWorld {
+    private WebDriver browser;
+
+    @BeforeClass
+    void testSetup(){
+	    WebDriverManager.chromedriver().setup();
+        browser = new ChromeDriver();
+    }
+
+    @Before
+    public void setup(){
+        browser.get("http://training-app.int.thetestpeople.com:8080/");
+    }
+
+    @Test
+    public void checkHelloWorldPrintedToOutputBox(){
+        WebElement outputBox = browser.findElement(By.cssSelector("#output-box"));
+        assertEquals(outputBox.getText(),"Hello World!");
+    }
+
 }
