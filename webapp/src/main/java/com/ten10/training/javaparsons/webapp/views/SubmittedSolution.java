c59427bd54f2d1d48d5dbac253a89e7211a3b7ff
Oliver Hoskins <oliver.hoskins@ten10.com>
2018-12-04 15:35:30 +0000

    I made a HTML file with Javascript inline to produce a basic website with no css. the javascript posts the text within the text area box with id:"input-box" to the URL: localhost:8080/exercise1 in JSON format, which then responds with a JSON formatted reponce found in the results class. Once a Post request has been accepted with a status of 200 "OK" the text "Correct Answer Well Done!" is written to the text area with the id:"output-box".

---
+++ webapp/src/main/java/com/ten10/training/javaparsons/webapp/views/SubmittedSolution.java
@@ -0,0 +1,13 @@
+package com.ten10.training.javaparsons.webapp.views;
+
+public class SubmittedSolution {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
