0e144f71e3c703bb6cbd5564c2015ea5dad25e46
Peter Russell <peter.russell@ten10.com>
2018-12-07 17:18:41 +0000

    Add a method to SolutionCompiler.CompilableSolution
    Also added a failing test for the Compiler. Make it pass!

--- compiler/src/main/java/com/ten10/training/javaparsons/compiler/impl/JavaSolutionCompiler.java
+++ compiler/src/main/java/com/ten10/training/javaparsons/compiler/impl/JavaSolutionCompiler.java
@@ -1,8 +1,6 @@
 package com.ten10.training.javaparsons.compiler.impl;
 
 import com.ten10.training.javaparsons.ErrorCollector;
-import com.ten10.training.javaparsons.Exercise;
-import com.ten10.training.javaparsons.Solution;
 import com.ten10.training.javaparsons.compiler.SolutionCompiler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +10,9 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
- * <p>Facade over the classes in javax.tools, particularly {@see javax.tools.JavaCompiler}</p>
+ * <p>Facade over the classes in javax.tools, particularly {@link javax.tools.JavaCompiler}</p>
  *
  * <p>Input files and class files are stored in memory</p>
  */
@@ -32,11 +29,8 @@ public class JavaSolutionCompiler implements SolutionCompiler {
 
     @Override
     public boolean compile(final CompilableSolution solution, final ErrorCollector errorCollector) {
-
         Objects.requireNonNull(solution, "solution");
         Objects.requireNonNull(errorCollector, "errorCollector");
-
-        final CharSequence classText = solution.getFullClassText();
 
         LOGGER.info("Compiling {}", solution);
 
@@ -44,14 +38,11 @@ public class JavaSolutionCompiler implements SolutionCompiler {
         List<SimpleJavaFileObject> compilationUnits = Collections.singletonList(new SolutionJavaFile(solution));
 
         try (JavaFileManager fileManager = new InMemoryFileManager(compiler.getStandardFileManager(diagnostics, null, null))) {
-
             JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
             return task.call();
-
         } catch (IOException e) {
             LOGGER.error("Error closing fileManager", e);
             throw new RuntimeException(e);
         }
-
     }
 }
