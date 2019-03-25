1a502a446de4c77a569743f253de97eab5bb330c
Ruaridh James <ruaridh.james@ten10.com>
2019-03-25 10:56:49 +0000

    Removed Integer Exercises.

--- exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
+++ exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
@@ -4,7 +4,6 @@ import com.ten10.training.javaparsons.Exercise;
 import com.ten10.training.javaparsons.ExerciseRepository;
 import com.ten10.training.javaparsons.compiler.SolutionCompiler;
 import com.ten10.training.javaparsons.impl.ExerciseList.PrintOutExercise;
-import com.ten10.training.javaparsons.impl.ExerciseList.ReturnIntegerExercise;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,9 +16,8 @@ public class ExerciseRepositoryImpl implements ExerciseRepository {
     public ExerciseRepositoryImpl(SolutionCompiler compiler) {
         PrintOutExercise helloWorld = new PrintOutExercise(compiler, "Hello World!", "Hello World!", 1);
         PrintOutExercise cruelWorld = new PrintOutExercise(compiler, "Goodbye Cruel World!", "Goodbye Cruel World!", 2);
-        ReturnIntegerExercise returnIntegerExercise = new ReturnIntegerExercise(compiler, 5, "return the integer 5", 3);
         this.compiler = compiler;
-        exercises = Arrays.asList(helloWorld, cruelWorld, returnIntegerExercise);
+        exercises = Arrays.asList(helloWorld, cruelWorld);
     }
 
     @Override
