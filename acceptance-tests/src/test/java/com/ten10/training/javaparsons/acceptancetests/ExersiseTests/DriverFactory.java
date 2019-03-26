14da5cf98b3e0b2bb454b7af386dbf3c45a65218
Ruaridh James <ruaridh.james@ten10.com>
2019-03-26 13:32:50 +0000

    Fixed acceptance tests for Multiple Exercises.

--- acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/DriverFactory.java
+++ acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/DriverFactory.java
@@ -22,7 +22,6 @@ class DriverFactory {
     }
 
     private void setDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Selenium\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
