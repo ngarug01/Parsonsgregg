09d68154b062e918a4f17d8d44668c2ca4cbd5ed
Rahat Shah <rahat.shah@ten10.com>
2019-03-27 10:39:46 +0000

    added javadocs to WebApp

--- webapp/src/main/java/com/ten10/training/javaparsons/webapp/controllers/ExerciseController.java
+++ webapp/src/main/java/com/ten10/training/javaparsons/webapp/controllers/ExerciseController.java
@@ -21,6 +21,7 @@ public class ExerciseController {
     @Autowired
     ExerciseRepository exerciseRepository;
 
+
     public static class ExerciseInformation {
         private final String url;
         private final String title;
@@ -32,21 +33,38 @@ public class ExerciseController {
             this.description = description;
         }
 
+        /** gets the title
+         * @return the title
+         */
         public String getTitle() {
             return title;
         }
 
+        /**
+         * gets the Url
+         * @return the Url
+         */
         public String getUrl() {
             return url;
         }
+
+        /**
+         * gets the description
+         * @return the description
+         */
         public String getDescription() {
             return description;
         }
     }
 
+
+    /** if getExercises is called, a get request is made from the user to Springboot
+     * Springboot then gets the list of all the exercises
+     * @return a list of all exercises with the title and description
+     */
     @RequestMapping(value = "getExercises", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<ExerciseInformation> geExercises() {
+    public List<ExerciseInformation> getExercises() {
         List<ExerciseInformation> combinedExerciseParameters = new ArrayList<>();
         for (int i = 1; i <= exerciseRepository.getExerciseArraySize(); i++) {
             Exercise exercise = exerciseRepository.getExerciseByIdentifier(i);
@@ -57,6 +75,13 @@ public class ExerciseController {
         return combinedExerciseParameters;
     }
 
+
+    /** if postRequestExercise is called, the user sends a sumbitted to solution to Springboot
+     * Springboot then compares the sumbission to the expected answer
+     * @param exercise the exercise type
+     * @param submittedSolution is the solution submitted by the user
+     * @return the results of whether or not the submission was correct or not
+     */
     @RequestMapping(value ="{identifier}", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     @ResponseBody
     public Results postRequestExercise(
