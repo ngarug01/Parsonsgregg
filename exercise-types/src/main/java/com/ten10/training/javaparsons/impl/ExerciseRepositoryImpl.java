377b7aefb8004bf0ad517622bdb6aa8c5ef7042c
Casey <casey.prosser@ten10.com>
2019-03-27 15:40:43 +0000

    amends passed in code from java linebreaks to html linebreaks for displaying in test-harness

--- exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
+++ exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
@@ -21,7 +21,7 @@ public class ExerciseRepositoryImpl implements ExerciseRepository {
     public ExerciseRepositoryImpl(SolutionCompiler compiler) {
         PrintOutExercise helloWorld = new PrintOutExercise(compiler, "Hello World!", "Hello World!", 1);
         PrintOutExercise cruelWorld = new PrintOutExercise(compiler, "Goodbye Cruel World!", "Goodbye Cruel World!", 2);
-        MethodsStatementsExercise methodStatementsHelloWorld = new MethodsStatementsExercise(compiler, "public class Main { /n public static void main { /n", "} /n }", "Hello World!", "Methods Statements Hello World", 3);
+        MethodsStatementsExercise methodStatementsHelloWorld = new MethodsStatementsExercise(compiler, " public class Main { /n public static void main (String[] args) {", " } /n }", "Hello World!", "Methods Statements Hello World", 3);
         this.compiler = compiler;
         exercises = Arrays.asList(helloWorld, cruelWorld, methodStatementsHelloWorld);
 
