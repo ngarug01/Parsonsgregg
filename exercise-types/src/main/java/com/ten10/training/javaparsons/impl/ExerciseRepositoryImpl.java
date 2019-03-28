ee2a0ee86c9c08154d8968d64741c7fd2d2208cb
Ruaridh James <ruaridh.james@ten10.com>
2019-03-28 12:25:54 +0000

    Merge branch 'ReturnTypeExercises-9.0' with 'StaticFieldExercises10.0'

--- exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
+++ exercise-types/src/main/java/com/ten10/training/javaparsons/impl/ExerciseRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.ten10.training.javaparsons.ExerciseRepository;
 import com.ten10.training.javaparsons.compiler.SolutionCompiler;
 import com.ten10.training.javaparsons.impl.ExerciseList.PrintOutExercise;
 import com.ten10.training.javaparsons.impl.ExerciseList.StaticFieldExercise;
+import com.ten10.training.javaparsons.impl.ExerciseList.ReturnTypeExercise;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,8 +23,9 @@ public class ExerciseRepositoryImpl implements ExerciseRepository {
         PrintOutExercise helloWorld = new PrintOutExercise(compiler, "Hello World!", "Hello World!", 1, "Write a Java code which when run will produce a string which reads");
         PrintOutExercise cruelWorld = new PrintOutExercise(compiler, "Goodbye Cruel World!", "Goodbye Cruel World!", 2, "Write a Java code which when run will produce a string which reads");
         StaticFieldExercise staticField = new StaticFieldExercise(compiler, 42, "42", 3, "Write a class which contains a static field with the value ");
+        ReturnTypeExercise returnSquareNumber = new ReturnTypeExercise(compiler, 4, "Two Squared", 4, "Write a Java method which when run will return");
         this.compiler = compiler;
-        exercises = Arrays.asList(helloWorld, cruelWorld, staticField);
+        exercises = Arrays.asList(helloWorld, cruelWorld, staticField, returnSquareNumber);
 
     }
 
--- exercise-types/src/test/java/com/ten10/training/javaparsons/impl/ReturnTypeExerciseSolutionTest.java
+++ exercise-types/src/test/java/com/ten10/training/javaparsons/impl/ReturnTypeExerciseSolutionTest.java
@@ -3,7 +3,7 @@ package com.ten10.training.javaparsons.impl;
 import com.ten10.training.javaparsons.ProgressReporter;
 import com.ten10.training.javaparsons.compiler.SolutionCompiler;
 import com.ten10.training.javaparsons.compiler.impl.JavaSolutionCompiler;
-import com.ten10.training.javaparsons.impl.ExerciseSolutions.PrintOutExerciseSolution;
+import com.ten10.training.javaparsons.impl.ExerciseSolutions.ReturnTypeExerciseSolution;
 import com.ten10.training.javaparsons.runner.SolutionRunner;
 import com.ten10.training.javaparsons.runner.impl.ThreadSolutionRunner;
 import org.junit.jupiter.api.Test;
@@ -11,73 +11,69 @@ import org.junit.jupiter.api.Test;
 import javax.tools.ToolProvider;
 
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-class PrintOutExerciseSolutionTest {
+class ReturnTypeExerciseSolutionTest {
     private SolutionCompiler compiler = mock(SolutionCompiler.class);
     private ThreadSolutionRunner runner = new ThreadSolutionRunner();
     private ThreadSolutionRunner mockRunner = mock(ThreadSolutionRunner.class);
     private ProgressReporter progressReporter = mock(ProgressReporter.class);
-    private final PrintOutExerciseSolution printOutExerciseSolution = new PrintOutExerciseSolution(compiler, runner, "userInput string inputted into solution", "Hello World!", progressReporter);
+    private final ReturnTypeExerciseSolution returnTypeExerciseSolution = new ReturnTypeExerciseSolution(compiler, runner,"userInput string inputted into solution",12, progressReporter);
+    private SolutionCompiler.CompilableSolution compilableSolution = mock(SolutionCompiler.CompilableSolution.class);
     private ClassLoader classLoader = mock(ClassLoader.class);
     private SolutionRunner.EntryPoint entryPoint = mock(SolutionRunner.EntryPoint.class);
 
 
-    @Test
+   @Test
     void checkEvaluate() throws Exception {
-        printOutExerciseSolution.evaluate();
-        verify(compiler).compile(printOutExerciseSolution, progressReporter);
+        returnTypeExerciseSolution.evaluate();
+        verify(compiler).compile(returnTypeExerciseSolution, progressReporter);
     }
 
     @Test
-    void doesNotRunWhenCompileFails() throws InterruptedException, ExecutionException, ReflectiveOperationException {
-        when(compiler.compile(printOutExerciseSolution, progressReporter)).thenReturn(false);
+    void earlyReturnWhenCompileFails() throws Exception {
+        when(compiler.compile(compilableSolution, progressReporter)).thenReturn(false);
+        returnTypeExerciseSolution.evaluate();
         verify(mockRunner, never()).run(classLoader, entryPoint, progressReporter);
     }
 
-
     @Test
     void checkGetFullClassText() {
-        assertEquals("userInput string inputted into solution", printOutExerciseSolution.getFullClassText());
+        assertEquals("userInput string inputted into solution", returnTypeExerciseSolution.getFullClassText());
     }
 
     @Test
     void checkGetClassName() {
-        assertEquals("Main", printOutExerciseSolution.getClassName());
+        assertEquals("Main", returnTypeExerciseSolution.getClassName());
     }
 
     @Test
     void evaluateFailsOnCompileClassNameIncorrect() throws Exception {
         SolutionCompiler compiler = new JavaSolutionCompiler(ToolProvider.getSystemJavaCompiler());
-        String userInput = "public class ain{\npublic static void main(String[] args){\nSystem.out.println(\"Pie\");}}";
-        PrintOutExerciseSolution printOutExerciseSolution = new PrintOutExerciseSolution(compiler, runner, userInput, "Pie", progressReporter);
-        printOutExerciseSolution.evaluate();
+        String userInput = "public class ain{\npublic Integer main(String[] args){return 12;}}";
+        ReturnTypeExerciseSolution returnTypeExerciseSolution = new ReturnTypeExerciseSolution(compiler, runner, userInput, 12, progressReporter);
+        returnTypeExerciseSolution.evaluate();
         verify(progressReporter).reportCompilerError(1, "class ain is public, should be declared in a file named ain.java");
     }
 
     @Test
     void evaluateFailsOnIncorrectAnswer() throws Exception {
         SolutionCompiler compiler = new JavaSolutionCompiler(ToolProvider.getSystemJavaCompiler());
-        String userInput = "public class Main{\npublic static void main(String[] args){\nSystem.out.println(\"Pie\");}}";
-        PrintOutExerciseSolution printOutExerciseSolution = new PrintOutExerciseSolution(compiler, runner, userInput, "Potato", progressReporter);
-
-        assertFalse(printOutExerciseSolution.evaluate());
+        String userInput = "public class Main{\npublic Integer main(String[] args){return 10;}}";
+        ReturnTypeExerciseSolution returnTypeExerciseSolution = new ReturnTypeExerciseSolution(compiler, runner, userInput, 12, progressReporter);
+        assertFalse(returnTypeExerciseSolution.evaluate());
     }
 
     @Test
     void evaluatePasses() throws Exception {
         SolutionCompiler compiler = new JavaSolutionCompiler(ToolProvider.getSystemJavaCompiler());
-        String userInput = "public class Main{\npublic static void main(String[] args){\nSystem.out.println(\"Pie\");}}";
-        PrintOutExerciseSolution printOutExerciseSolution = new PrintOutExerciseSolution(compiler, runner, userInput, "Pie", progressReporter);
-        assertTrue(printOutExerciseSolution.evaluate());
-
+        String userInput = "public class Main{\npublic Integer main(String[] args){return 12;}}";
+        ReturnTypeExerciseSolution returnTypeExerciseSolution = new ReturnTypeExerciseSolution(compiler, runner, userInput, 12, progressReporter);
+        assertTrue(returnTypeExerciseSolution.evaluate());
     }
 }
 
