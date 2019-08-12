038074112bfa4c4424094a81f52bcea874cd4ca1
mohammed.mumtaz <mohammed.mumtaz@ten10.com>
2019-08-12 10:04:26 +0100

    Refactor acceptance tests
    Removing multiple object pages for each acceptance test and refactoring to use a common object page.(tidy up imports)

--- acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/HelloWorldAT.java
+++ acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/HelloWorldAT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.hamcrest.MatcherAssert.assertThat;
--- acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/ReturnCharAAT.java
+++ acceptance-tests/src/test/java/com/ten10/training/javaparsons/acceptancetests/ExersiseTests/ReturnCharAAT.java
@@ -8,11 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
