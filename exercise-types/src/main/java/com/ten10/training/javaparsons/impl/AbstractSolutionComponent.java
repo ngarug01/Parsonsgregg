2c1c988ca6770ae8f78aa736f646e9f951ad022e
Peter Russell <peter.russell@ten10.com>
2019-11-14 12:25:58 +0000

    Add skeleton types for the SolutionComponents.

---
+++ exercise-types/src/main/java/com/ten10/training/javaparsons/impl/AbstractSolutionComponent.java
@@ -0,0 +1,43 @@
+package com.ten10.training.javaparsons.impl;
+
+import com.ten10.training.javaparsons.ProgressReporter;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.unmodifiableList;
+
+/**
+ * Abstract baseclass for Solution components that delegate to other solution components
+ * @param <I> The input type
+ * @param <O> The output type - the type passed down the chain.
+ */
+public abstract class AbstractSolutionComponent<I, O> implements SolutionComponent<I> {
+
+    private final List<SolutionComponent<O>> successors;
+
+    AbstractSolutionComponent(List<SolutionComponent<O>> successors) {
+        this.successors = unmodifiableList(successors);
+    }
+
+    final boolean feedValueDown(O value, ProgressReporter progressReporter) {
+        for (SolutionComponent<O> successor : successors) {
+            if(!successor.evaluate(value, progressReporter)) { return false;}
+        }
+        return true;
+    }
+
+    ProgressReporter decorateProgressReporter(ProgressReporter progressReporter) {
+        return progressReporter;
+    }
+
+    abstract Optional<O> generateOutput(I input, ProgressReporter progressReporter);
+
+    @Override
+    public final boolean evaluate(I input, ProgressReporter progressReporter) {
+        Optional<O> output = generateOutput(input, progressReporter);
+        if (!output.isPresent()) return false;
+        progressReporter = decorateProgressReporter(progressReporter);
+        return feedValueDown(output.get(), progressReporter);
+    }
+}
---
+++ exercise-types/src/main/java/com/ten10/training/javaparsons/impl/SolutionComponent.java
@@ -0,0 +1,17 @@
+package com.ten10.training.javaparsons.impl;
+
+import com.ten10.training.javaparsons.ProgressReporter;
+
+/**
+ * Type definining part of a chain of solution components
+ * @param <I> The type of the input
+ */
+public interface SolutionComponent<I> {
+    /**
+     * Evaluate input returning true if it is successful.
+     * @param input
+     * @param progressReporter
+     * @return
+     */
+    boolean evaluate(I input, ProgressReporter progressReporter);
+}
